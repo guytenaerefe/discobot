ed0ca68e3f366ee9199d371d5353bbfdaa97f31c
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-24 00:51:04 +0100

    Initial checkin

---
+++ README.md
@@ -0,0 +1,4 @@
+# Discobot
+
+A Discord bot using [`discord.js`](https://github.com/hydrabolt/discord.js/).
+The environment variables are loaded for authenticating, using the [DotEnv](https://www.npmjs.com/package/dotenv) package. 
---
+++ bot.js
@@ -0,0 +1,41 @@
+var Discord = require("discord.js");
+stackexchange = require('stackexchange');
+require('dotenv').config();
+ 
+var options = { version: 2.2 }
+var context = new stackexchange(options);
+
+discoBot = new Discord.Client();
+//Debug and warning handlers, these log debug messages and warnings to console
+discoBot.on("debug", (m) => console.log("[debug]", m));
+discoBot.on("warn", (m) => console.log("[warn]", m));
+
+discoBot.on("message", function(message){
+    if (message.content === "who is the best") {
+        discoBot.reply(message, "brix is the best");
+    } else if (message.content === 'what did you say') {
+        discoBot.reply(message, 'we\'re taking the bots to isenguard');
+    } else if (message.content.indexOf('stackoverflow' > -1)) {
+
+        var filter = {
+            key: 'YOUR_API_KEY',
+            pagesize: 55555,
+            tagged: 'php',
+            sort: 'activity',
+            order: 'asc'
+        };
+        context.questions.questions(filter, function(err, results) {
+            if (err) throw err;
+        
+            console.log(results.items);
+            console.log(results.has_more);
+        });        
+
+    }
+
+});
+
+/**
+ * Login
+ */
+discoBot.login(process.env.USERNAME, process.env.PASSWORD).catch((e) => console.log(e));;
