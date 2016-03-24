c8d50dc4fc53bc9f51dcd3a62f07954de95af3e2
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-24 10:38:03 +0100

    Moved bot

--- bot/bot.js
+++ bot/bot.js
@@ -7,28 +7,35 @@ var context = new stackexchange(options);
 
 discoBot = new Discord.Client();
 //Debug and warning handlers, these log debug messages and warnings to console
-discoBot.on("debug", (m) => console.log("[debug]", m));
-discoBot.on("warn", (m) => console.log("[warn]", m));
+discoBot.on('debug', (m) => console.log('[debug]', m));
+discoBot.on('warn', (m) => console.log('[warn]', m));
 
-discoBot.on("message", function(message){
-    if (message.content === "who is the best") {
-        discoBot.reply(message, "brix is the best");
+discoBot.on('message', function(message){
+    if (message.content === "quit") {
+        discoBot.reply(message, 'ok bye');
+        process.exit();
     } else if (message.content === 'what did you say') {
         discoBot.reply(message, 'we\'re taking the bots to isenguard');
-    } else if (message.content.indexOf('stackoverflow' == 0)) {
+    } else if (message.content.indexOf('stackoverflow') == 0) {
+
+        words = message.content.replace('stackoverflow', '');
+        console.log(words);
 
         var filter = {
             key: process.env.SE_API_KEY,
-            pagesize: 5,
-            tagged: 'php',
-            sort: 'activity',
-            order: 'asc'
+            pagesize: 2,
+            //tagged: 'php',
+            intitle: words,
+            sort: 'score',
+            order: 'desc',
+            site: 'stackoverflow'
         };
-        context.questions.questions(filter, function(err, results) {
+
+        //context.questions.questions(filter, function(err, results) {
+        context.search.search(filter, function(err, results) {
             if (err) throw err;
         
             console.log(results.items);
-            console.log(results.has_more);
         });        
 
     }
